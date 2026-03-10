7a9c3ecb2f189849ec45de2ad132b95f464a6156
Dmitry Marchenko <>
2026-03-10 07:58:45 +0300

    pure C++ class testing

---
+++ Source/AutomationTesting/Items/Battery.cpp
@@ -0,0 +1,42 @@
+﻿#include "Battery.h"
+
+//using namespace TPS;
+
+constexpr float ChargeAmount = 0.1f;
+
+Battery::Battery(float InPercent)
+{
+	SetPercent(InPercent);
+}
+
+void Battery::Charge()
+{
+	SetPercent(GetPercent() + ChargeAmount);
+}
+
+void Battery::UnCharge()
+{
+	SetPercent(GetPercent() - ChargeAmount);
+}
+
+float Battery::GetPercent() const
+{
+	return Percent;
+}
+
+FColor Battery::GetColor() const
+{
+	if (GetPercent() > 0.8f) return FColor::Green;
+	if (GetPercent() > 0.3f) return FColor::Yellow;
+	return FColor::Red;
+}
+
+FString Battery::ToString() const
+{
+	return FString::Printf(TEXT("%i%%"), FMath::RoundToInt(GetPercent() * 100.0f));
+}
+
+void Battery::SetPercent(float InPercent)
+{
+	Percent = FMath::Clamp(InPercent, 0.0f, 1.0f);
+}
---
+++ Source/AutomationTesting/Items/Battery.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+#include "CoreMinimal.h"
+
+class AUTOMATIONTESTING_API Battery
+{
+public:
+	Battery() = default;
+	Battery(float InPercent);
+
+	void Charge();
+	void UnCharge();
+
+	float GetPercent() const;
+	FColor GetColor() const;
+	FString ToString() const;
+
+	bool operator >=(const Battery& rhs) const { return GetPercent() >= rhs.GetPercent(); }
+	bool operator <(const Battery& rhs) const { return GetPercent() < rhs.GetPercent(); }
+	bool operator ==(const Battery& rhs) const { return FMath::IsNearlyEqual(GetPercent(), rhs.GetPercent()); }
+
+private:
+	float Percent = 1.0f;
+	void SetPercent(float InPercent);
+};
